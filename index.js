a873c95968f24c4488f342709383698a33648ca6
vikash surin <vikashsurin10@gmail.com>
2019-06-01 10:25:18 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express"); //common js module only in - node
+
+const app = express();
+
+//route handler
+app.get("/", (req, res) => {
+	//app - express server
+	res.send("hi there"); //get - route for https req
+}); // '/' - watch for req in /
+
+//run the server
+const PORT = process.env.PORT || 5000; //ability to inject env variables dynamically
+app.listen(PORT);
